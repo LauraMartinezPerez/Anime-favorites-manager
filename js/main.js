033620725a41be5e28fafbf2254ae566ce3c8a80
Laura <laura.laukiz@gmail.com>
2025-03-14 09:43:03 +0100

    maquetando 1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const list = document.querySelector(".js-list");
+const searchButton = document.querySelector(".js-button");
+const searchValue = document.querySelector(".js-search")
+
+fetch("https://api.jikan.moe/v4/anime?q=naruto")
+.then(response => response.json())
+.then(data => {
+   
+})
+
