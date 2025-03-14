be4ed83d4db1c46e266a0ec4df884a7a5b5f6b4e
Laura <laura.laukiz@gmail.com>
2025-03-14 11:20:33 +0100

    2. Busqueda

--- js/main.js
+++ js/main.js
@@ -2,11 +2,40 @@
 
 const list = document.querySelector(".js-list");
 const searchButton = document.querySelector(".js-button");
-const searchValue = document.querySelector(".js-search")
+const searchInput = document.querySelector(".js-search")
 
-fetch("https://api.jikan.moe/v4/anime?q=naruto")
+const renderLi = (animeData) => {
+    list.innerHTML = "";
+    for(const serie of animeData) {
+        if (serie.images.jpg.image_url === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") {
+            `<li>
+        <img class="img" src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg">
+        <p class="title">${serie.title}</p>
+        </li>`
+        } else {
+        list.innerHTML += 
+        `<li>
+        <img class="img" src="${serie.images.jpg.image_url}">
+        <p class="title">${serie.title}</p>
+        </li>`
+    }}
+
+}
+
+
+const handleclick = (ev) => {
+    ev.preventDefault();
+    const searchValue = searchInput.value;
+    fetch(`https://api.jikan.moe/v4/anime?q=${searchValue}`)
 .then(response => response.json())
-.then(data => {
+.then(patata => {
+    console.log(patata);
+    renderLi (patata.data);
+
    
 })
 
+  
+};
+
+searchButton.addEventListener("click", handleclick);
