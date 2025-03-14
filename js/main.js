7b78e8cdd1f00bd27dc9109ebab103fdbc20b235
Laura <laura.laukiz@gmail.com>
2025-03-14 14:50:03 +0100

    3.Favoritos falta css

--- js/main.js
+++ js/main.js
@@ -3,10 +3,13 @@
 const list = document.querySelector(".js-list");
 const searchButton = document.querySelector(".js-button");
 const searchInput = document.querySelector(".js-search");
-const favoriteList = document.querySelector(".js-favorites-list");
+const containerFavoriteList = document.querySelector(".js-favorites-list");
+let favoriteList = []; // este es el array que guarda los animes favoritos
+let allAnimesData = []; // este es el array global que guarda todos los animes
 
 //Pintar del servidor segun busqueda --> de lo pintado seleccionar fav
 const renderLi = (animeData) => {
+    allAnimesData = animeData;
     list.innerHTML = "";
     for(const serie of animeData) {
         if (serie.images.jpg.image_url === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") { 
@@ -47,7 +50,36 @@ searchButton.addEventListener("click", searchclick);
 // Funcion seleccionar fav
 
 const handleFavorite = (event) => {
-    console.log(event.currentTarget.id);
+    const idAnimeClicked = parseInt(event.currentTarget.id);
+/*     console.log(idAnimeClicked); */
 
-    
+    //busco el anime clickado a partir de el id devuelto ---const idAnimeClicked---
+const animeSelected = allAnimesData.find((anime) => {
+    return anime.mal_id === idAnimeClicked;
+})
+
+/* console.log(animeSelected); */
+
+    //añadir ese anime a la lista de favoritos ---let favoriteList = [];---
+
+   favoriteList.push(animeSelected);
+   /* console.log(favoriteList); */
+   containerFavoriteList.innerHTML = "";
+   for(const serie of favoriteList) {
+   
+        if (serie.images.jpg.image_url === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png") { 
+            containerFavoriteList.innerHTML += 
+            `<li class="js-anime-img" id=${serie.mal_id}>
+                <img class="img"  src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg">
+                <p class="title">${serie.title}</p>
+            </li>`
+        } else {
+            containerFavoriteList.innerHTML += 
+            `<li class="js-anime-img" id=${serie.mal_id}>
+                <img class="img"   src="${serie.images.jpg.image_url}">
+                <p class="title">${serie.title}</p>
+            </li>`
+
+        } 
+    }
 };
