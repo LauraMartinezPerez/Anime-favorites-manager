469047532a42c94147b2ca62f8fdd1be6c9564a0
Laura <laura.laukiz@gmail.com>
2025-03-16 12:27:44 +0100

    Bonus 5 OK

--- js/main.js
+++ js/main.js
@@ -19,47 +19,44 @@ const renderFavoriteList = () => {
             `<li class="js-anime-img anime-fav" id=${serie.mal_id}>
                  <img class="img-fav"  src="https://www.svgrepo.com/show/508699/landscape-placeholder.svg">
                     <p class="title-fav">${serie.title}</p>
-                   <i class="fa-solid fa-circle-xmark js-x id=${serie.mal_id}"></i>
+                   <i class="fa-solid fa-circle-xmark js-x" id="${serie.mal_id}"></i>
             </li>`
         } else {
             containerFavoriteList.innerHTML += 
             `<li class="js-anime-img anime-fav" id=${serie.mal_id}>
                 <img class="img-fav"   src="${serie.images.jpg.image_url}">
                 <p class="title-fav">${serie.title}</p> 
-                <i class="fa-solid fa-circle-xmark js-x id=${serie.mal_id}"></i>
+                <i class="fa-solid fa-circle-xmark js-x" id="${serie.mal_id}"></i>
             </li>`
         } 
-        const xIcon = document.querySelector(".js-x");
-        xIcon.addEventListener("click", handleXIcon);
+        // Traigo los iconos X y hago evento clic (handleXIcon) a todos los iconos X que lleven clase "js-x"
+        const xIcon = document.querySelectorAll(".js-x");
+        for (const closeIcon of xIcon) {
+            closeIcon.addEventListener("click", handleXIcon);
+        }
     }
 };
 
-// Funcion seleccionar fav
+// Funcion eliminar favoritos
 const handleXIcon = (event) => {
-    console.log("ha hecho clic");
+    event.preventDefault();
     const idfavAnimeClicked = parseInt(event.currentTarget.id);
 
-    //busco el anime clickado a partir de el id devuelto ---const idfavAnimeClicked---
-    const favAnimeSelected = favoriteList.find((anime) => {
-        return anime.mal_id === idfavAnimeClicked;
-    })
+    //busco el anime clickado (uso findIndex xq si no cuando use el splice me va a eliminar el primer elemento que cumple la condicion y lo que quiero es que me localice la posicion del elemento clickado en el array para eliminar ese en concreto
+    const favAnimeSelected = favoriteList.findIndex((anime) => 
+        anime.mal_id === idfavAnimeClicked
+    );
 
+    //entro en la lista de favoritos y elimino el anime clickado
+        favoriteList.splice(favAnimeSelected, 1);
+    
 
-    //añadir ese anime a la lista de favoritos ---let favoriteList = [];---
-    favoriteList.splice(favAnimeSelected);
+    //vuelvo a guardar la lista de favoritos actualizada
+    localStorage.setItem("favoritesAnimes", JSON.stringify(favoriteList));
 
-    //Cuando vaya añadiendo favoritas a mi lista, voy guardando la lista en el localStorage
-    localStorage.removeItem("favoritesAnimes");
-
-    //Pinto la lista de favoritos
+    //Pinto la lista de favoritos actualizada
     renderFavoriteList();
 };
-
-
-
-
-
-
 
 
 
@@ -70,6 +67,22 @@ if (favoritesLocalStorage !== null) {
     favoriteList = JSON.parse(favoritesLocalStorage);
     renderFavoriteList();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //Pintar del servidor segun busqueda --> de lo pintado seleccionar fav
